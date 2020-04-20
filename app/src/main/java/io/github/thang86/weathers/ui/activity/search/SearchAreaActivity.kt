4500efe0461089dee33ebd69740079bb98806335
thang86 <tranthang8696@gmail.com>
2020-04-20 18:30:35 +0700

    Apply extension

--- app/src/main/java/io/github/thang86/weathers/ui/activity/search/SearchAreaActivity.kt
+++ app/src/main/java/io/github/thang86/weathers/ui/activity/search/SearchAreaActivity.kt
@@ -2,15 +2,25 @@ package io.github.thang86.weathers.ui.activity.search
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.places.Place
+import com.google.android.gms.location.places.Places
+import com.google.android.gms.location.places.ui.PlaceSelectionListener
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import io.github.thang86.weathers.R
+import java.util.*
+
 
 class SearchAreaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_area)
+
     }
 
     companion object{
